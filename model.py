ae73c383c453e8e6ac1c0a260dee82d81cf5e309
realBarry123 <barryyu28@gmail.com>
2024-05-18 19:48:49 -0700

    Fix Generator architecture

--- model.py
+++ model.py
@@ -31,12 +31,27 @@ class Generator(nn.Module):
             nn.Unflatten(dim=0, unflattened_size=(7, 7, 1)),
             PrintShape()
         )
+        '''
         self.upscale = nn.Sequential(
-            nn.ConvTranspose2d(in_channels=129, out_channels=128, kernel_size=4, stride=2, bias=False),
+            PrintShape(),
+            nn.ConvTranspose2d(in_channels=7, out_channels=7, kernel_size=4, stride=2, bias=False),
             nn.ReLU(),
-            nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=4, stride=2, bias=False),
+            PrintShape(),
+            nn.ConvTranspose2d(in_channels=7, out_channels=7, kernel_size=4, stride=2, bias=False),
             nn.ReLU(),
-            nn.Conv2d(in_channels=128, out_channels=1, kernel_size=4, bias=False),
+            PrintShape(),
+            nn.Conv2d(in_channels=7, out_channels=1, kernel_size=4, bias=False),
+        )
+        '''
+        self.upscale = nn.Sequential(
+            PrintShape(),
+            nn.ConvTranspose2d(in_channels=129, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            PrintShape(),
+            nn.ConvTranspose2d(in_channels=128, out_channels=128, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            PrintShape(),
+            nn.Conv2d(in_channels=128, out_channels=1, kernel_size=1, stride=1),
         )
 
     def forward(self, latent_input, label_input):
@@ -45,5 +60,6 @@ class Generator(nn.Module):
         print(latent_output.shape)
         print(label_output.shape)
         concated_tensor = torch.cat((latent_output, label_output), dim=2)
-        print(concated_tensor.shape)
-        return self.upscale(concated_tensor)
+        concated_tensor = concated_tensor.unsqueeze(0)  # [7, 7, 129] -> [1, 7, 7, 129]
+        concated_tensor = concated_tensor.permute(0, 3, 1, 2)  # [1, 7, 7, 129] -> [1, 129, 7, 7]
+        return torch.squeeze(self.upscale(concated_tensor))
