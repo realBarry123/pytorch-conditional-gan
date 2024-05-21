202b95bafcb8a458716fdcf20f4c078f13f9931f
realBarry123 <barryyu28@gmail.com>
2024-05-21 16:07:36 -0700

    Initialize models for training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import torch
 import numpy
 
-from model import Generator, Discriminator
+from model import Generator, Discriminator, weights_init
 from torch.utils.data import TensorDataset, DataLoader
 from preprocessing import format_data
 
@@ -30,8 +30,19 @@ test_data = TensorDataset(test_images, test_labels)
 
 z = torch.randn(100)
 fixed_noise = torch.randn(64, 100, 1, 1, device="cpu")
+
+# Create our heroes
 netG = Generator(0).to("cpu")
 netD = Discriminator(0).to("cpu")
+
+try:
+    netG.load_state_dict(torch.load("Models/netG.pkl"))  # load netG weights
+    netD.load_state_dict(torch.load("Models/netD.pkl"))  # load netD weights
+except FileNotFoundError:
+    netG.apply(weights_init)
+    netD.apply(weights_init)
+finally:
+    print(":(")
 
 # print(netD(test_images[0], test_labels[0].int()))
 # plotImage(netG(z, test_labels[0]).detach().numpy())
@@ -46,3 +57,10 @@ optimizerG = torch.optim.Adam(netG.parameters(), lr=learning_rate, betas=(beta1,
 
 print("-=!Goblin Mode Activated!=-")
 
+for epoch in range(5):
+    # for each batch in the dataloader
+    for i, data in enumerate(dataloader, 0):
+
+        netD.zero_grad()
+
+print("-=.Goblin Mode Deactivated.=-")
--- model.py
+++ model.py
@@ -3,6 +3,15 @@ import torch.nn as nn
 import torch.nn.parallel
 
 
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:  # find convolutional layer
+        nn.init.normal_(m.weight.data, 0.0, 0.02)  # randomly initialize weights
+    elif classname.find('BatchNorm') != -1:  # find batchnorm
+        nn.init.normal_(m.weight.data, 1.0, 0.02)  # randomly initialize weights
+        nn.init.constant_(tensor=m.bias.data, val=0)  # set bias to 0
+
+
 class PrintShape(nn.Module):
     def __init__(self):
         super(PrintShape, self).__init__()
