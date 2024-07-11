9f5423e64514dacc7b9656eb86e7b391f378aa2c
realBarry123 <barryyu28@gmail.com>
2024-07-11 18:30:58 -0400

    Load data for classifier

--- train_c.py
+++ train_c.py
@@ -1,6 +1,12 @@
 import torch
-from train import train_loader, test_loader
 from model import Classifier, weights_init
+from torchvision import datasets, transforms
+
+batch_size = 128
+
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
 
 netC = Classifier(0).to("cpu")
 
@@ -8,6 +14,14 @@ try:
     netC.load_state_dict(torch.load("Models/netG.pkl"))  # load netC weights
 except FileNotFoundError:
     netC.apply(weights_init)
+
+# Download and load the training data
+train_set = datasets.MNIST('Datasets/mnist', download=True, train=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=batch_size, shuffle=True)
+
+# Download and load the test data
+test_set = datasets.MNIST('Datasets/mnist', download=True, train=False, transform=transform)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=batch_size, shuffle=True)
 
 for epoch in range(5):
 
@@ -17,3 +31,4 @@ for epoch in range(5):
         labels = data[1].to("cpu")
 
         netC.zero_grad()
+        netC(images)
